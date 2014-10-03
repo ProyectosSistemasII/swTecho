86d5e91b4ee48a322245ebbb46f395b9d08c7cb8
erickvr <erick.vr33@gmail.com>
2014-10-03 00:08:50 -0600

    diseños
    Diseños

--- TechoCeiva/TechoCeiva/UC_Insumos.xaml.cs
+++ TechoCeiva/TechoCeiva/UC_Insumos.xaml.cs
@@ -14,13 +14,29 @@ using System.Windows.Shapes;
 namespace TechoCeiva
 {
 	/// <summary>
-	/// Lógica de interacción para UC_Insumos.xaml
+	/// Lógica de interacción para UC_Tools.xaml
 	/// </summary>
-	public partial class UC_Insumos : UserControl
+	public partial class UC_Tools : UserControl
 	{
-		public UC_Insumos()
+		public UC_Tools()
 		{
 			this.InitializeComponent();
+			canvasContent.Children.Clear();
+			txtNulo.Focus();
+		}
+
+		private void btnAgregar_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// TODO: Agregar implementación de controlador de eventos aquí.
+			canvasContent.Children.Clear();
+			canvasContent.Children.Add(new UC_NewTool());
+		}
+
+		private void btnGestion_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// TODO: Agregar implementación de controlador de eventos aquí.
+			canvasContent.Children.Clear();
+			canvasContent.Children.Add(new UC_Manage());
 		}
 	}
 }
---
+++ TechoCeiva/TechoCeiva/UC_NuevoInsumo.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TechoCeiva
+{
+	/// <summary>
+	/// Lógica de interacción para UC_NewTool.xaml
+	/// </summary>
+	public partial class UC_NewTool : UserControl
+	{
+		public UC_NewTool()
+		{
+			this.InitializeComponent();
+		}
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            WinAddTool nWinToAddTool = new WinAddTool();
+            System.Windows.Forms.Integration.ElementHost.EnableModelessKeyboardInterop(nWinToAddTool);
+            nWinToAddTool.Show();
+        }
+	}
+}
