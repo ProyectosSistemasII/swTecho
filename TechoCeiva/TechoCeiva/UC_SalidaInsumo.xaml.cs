1d98bc0348f84a12a4eed46c0f69d5dd0ca136de
erickvr <erick.vr33@gmail.com>
2014-10-06 01:43:45 -0600

    Nuevos forms
    Nuevos formularios ya corregidos

---
+++ TechoCeiva/TechoCeiva/UC_SalidaInsumo.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TechoCeiva
+{
+	/// <summary>
+	/// Lógica de interacción para UC_SalidaInsumo.xaml
+	/// </summary>
+	public partial class UC_SalidaInsumo : UserControl
+	{
+		public UC_SalidaInsumo()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
